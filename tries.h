a58e55d6d24f2219544de28d451123dd6de27b44
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-11-01 18:53:25 +0700

    Finish up header file

--- tries.h
+++ tries.h
@@ -8,7 +8,8 @@ typedef struct trie {
 } trie;
 
 trie *trie_create();
-trie *trie_insert(trie *root, char *key, char value);
+trie *trie_insert(trie *root, char *key, char *value);
 trie *trie_lookup(trie *root, char *key);
 trie *trie_delete(trie *root, char *key);
+trie *trie_delete_dfs(trie *root, trie *parent, char *key, int *found);
 void trie_print(trie *root, int level);
