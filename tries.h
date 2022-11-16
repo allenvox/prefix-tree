c57130a467ad3aea1b76581978a36542a8f6f089
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-11-16 12:55:01 +0700

    Add min/max functions for ordered search tree

--- tries.h
+++ tries.h
@@ -4,12 +4,14 @@ typedef struct trie {
     char *value;
     char ch;
     struct trie *sibling; // linked list prototype
-    struct trie *child; // only first child - next are in siblings
+    struct trie *child;   // left child - right sibling
 } trie;
 
 trie *trie_create();
 trie *trie_insert(trie *root, char *key, char *value);
 trie *trie_lookup(trie *root, char *key);
+trie *trie_min(trie *root);
+trie *trie_max(trie *root);
 trie *trie_delete(trie *root, char *key);
 trie *trie_delete_dfs(trie *root, trie *parent, char *key, int *found);
 void trie_print(trie *root, int level);
